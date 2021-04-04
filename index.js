093424c48ad30046290d3f83cd635775a8784ad1
Marcus Mors <alivezeh@gmail.com>
2021-04-03 23:35:36 -0500

    calendar and events instances well parsed but the object is never shown again in MainProcess(where I sort the dates)

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ const { Calendar } = require("./calendar.js")
 const { futureDay, arrayIncludes } = require("./functions.js")
 const { authorize } = require("./gapiFunctions.js")
 const { doCopyCalendarEvents } = require("./copyCalendarEvents.js")
-let csv = require("csv")
-
+// let csv = require("csv")
+// master, main, core,
 // Load client secrets from a local file.
 fs.readFile("credentials.json", (err, content) => {
 	if (err) return console.log("Error loading client secret file:", err)
 	// Authorize a client with credentials, then call the Google Calendar API.
-	authorize(JSON.parse(content), copyExternalCalendars)
+	authorize(JSON.parse(content), main)
 })
+const DATA_PATH = "data.json"
 
-let calendars = []
-
-let lowestPriority = -1
-let totalCalendars = 0
+function setData(callback, auth) {
+	console.log("setData")
+	let data = getData(auth)
+	fs.writeFile(DATA_PATH, JSON.stringify(data), (err) => {
+		if (err) return console.error(err)
+		console.log("data stored to", DATA_PATH)
+	})
+	callback(data)
+}
 
 let wasLastEventOutside
 let headingHome
 
-let descriptionCounters = []
-
 const getData = async (auth) => {
 	try {
+		// calendars is the main variable
+		let calendars = {}
+		let lowestPriority = -1
+		let totalCalendars = 0
 		const calendar = google.calendar({ version: "v3", auth })
 		/*********************************************
 		 * Get Calendar Id's
@@ -48,7 +56,7 @@ const getData = async (auth) => {
 		}
 		// Turning the array into a 2D Array
 		for (let i = 0; i < lowestPriority + 1; i++) {
-			calendars[i] = []
+			calendars[i] = {}
 		}
 		console.log(`\ttotalCalendars : ${totalCalendars}`)
 		console.log(`\tlowestPriority : ${lowestPriority}`)
@@ -59,11 +67,17 @@ const getData = async (auth) => {
 		for (let i = 0; i < totalCalendars; i++) {
 			const indexHolder = itemIndexHolder[i]
 			const calendarItem = calendarListResponse.data.items[indexHolder]
+			let aux = 0
 			for (let j = 0; j <= +lowestPriority; j++) {
 				if (+calendarItem.summary.slice(-1) === j) {
-					calendars[j].push(
-						new Calendar(calendarItem.summary, calendarItem.id)
+					calendars[j][aux] = new Calendar(
+						calendarItem.summary,
+						calendarItem.id
 					)
+					// calendars[j].push(
+					// 	new Calendar(calendarItem.summary, calendarItem.id)
+					// )
+					aux++
 				}
 			}
 		}
@@ -82,8 +96,11 @@ const getData = async (auth) => {
 		 *********************************************/
 		console.log(`\nChecking all events in all calendars\n`)
 		for (let i = 0; i < +lowestPriority + 1; i++) {
-			for (let j = 0; j < calendars[i].length; j++) {
+			const calendarsLength = Object.keys(calendars[i]).length
+			for (let j = 0; j < calendarsLength; j++) {
+				// for (let j = 0; j < calendars[i].length; j++) {
 				const cal = calendars[i][j]
+				// const cal = calendars[i][j]
 				// console.log(`calendars[${i}][${j}] : ${cal.summary}`)
 				const listEventsResponse = await calendar.events.list({
 					calendarId: cal.id,
@@ -157,7 +174,7 @@ const getData = async (auth) => {
 								// console.log(`${start} - ${end} | ${summary}`)
 							}
 						}
-						descriptionCounters = []
+						// descriptionCounters = [] // useless? perhaps yes
 						// } else {
 						// console.log("No upcoming events found.")
 					}
@@ -170,12 +187,15 @@ const getData = async (auth) => {
 
 		//making sure the data structure is filled
 		console.log(`\n\nChecking calendars and its events\n`)
-		for (let i = 0; i < calendars.length; i++) {
-			for (let j = 0; j < calendars[i].length; j++) {
+		for (let i = 0; i < +lowestPriority + 1; i++) {
+			const calendarsLength = Object.keys(calendars[i]).length
+			for (let j = 0; j < calendarsLength; j++) {
+				// for (let j = 0; j < calendars[i].length; j++) {
 				const cal = calendars[i][j]
 				cal.print()
 			}
 		}
+		return calendars
 	} catch (error) {
 		console.error(error)
 	}
@@ -183,7 +203,7 @@ const getData = async (auth) => {
 
 let dayEvents = []
 
-function mainProcess(days) {
+function mainProcess(calendars, days = 14) {
 	// turn dayEvents into a 2D array
 	for (let i = 0; i < calendars.length; i++) {
 		dayEvents[i] = []
@@ -192,8 +212,11 @@ function mainProcess(days) {
 	 * Getting a specific day events
 	 *********************************************/
 	//get today events, from now until the end of the day
-	for (let i = 0; i < calendars.length; i++) {
-		for (let j = 0; j < calendars[i].length; j++) {
+	const length = Object.keys(calendars).length
+	for (let i = 0; i < length; i++) {
+		let calendarsLength = Object.keys(calendars[i]).length
+		// for (let i = 0; i < calendars.length; i++) {
+		for (let j = 0; j < calendarsLength; j++) {
 			const cal = calendars[i][j]
 			const startDate = futureDay(0)
 			const endDate = futureDay(1, false)
@@ -239,17 +262,22 @@ function mainProcess(days) {
 	// }
 }
 
-async function copyExternalCalendars(auth) {
+async function main(auth) {
 	try {
 		// console.log(`Copying external calendars...`)
 		// await doCopyCalendarEvents(auth)
 		// what to do if an event of the original calendar changes, is deleted, or a new one is added?
-		console.log(`standardizing the descriptions and first check...`)
-		await getData(auth)
-		const days = 14
-		await mainProcess(days)
-		// console.log(`descriptions...`)
-		// await getDay(auth)
+		// console.log(`standardizing the descriptions and first check...`)
+
+		/*********************************************
+		 * Save data to speed up the debugging process and to sell it later <3
+		 *********************************************/
+		fs.readFile(DATA_PATH, "utf-8", (err, data) => {
+			if (err) return setData(mainProcess, auth)
+			console.log("readFile")
+			let calendars = JSON.parse(data)
+			mainProcess(calendars)
+		})
 	} catch (error) {
 		console.error(error)
 	}
